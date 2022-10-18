5df19e7e61adba8e0e3975805958c47722bdcb51
BrayanCamilo16 <brayanpalomino535@gmail.com>
2022-10-17 22:38:39 -0500

    Terminado el portafolio

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/image', express.static(path.join(__dirname, 'public')))
 //que tipo de motor vamos a utilizaar
 app.set('view engine', 'ejs')
 //donde van a estra guardadas esas vistas
-app.set('views',__dirname+ '/views')
+app.set('views', __dirname + '/views')
 
 //aqui estan las rutas y van por el metodo get
 app.get('/', (req, res) => {
@@ -19,16 +19,29 @@ app.get('/', (req, res) => {
     console.log(__dirname)
     res.render('index')
 })
-app.get('/educacion', (req, res) => {
-    res.render('educacion'),{
+app.get('/Educacion', (req, res) => {
+    res.render('Educacion'), {
         titulo: "Esto es educacion",
         descripcion: "Aqui esta la formacion academica"
     }
-    })
+})
+app.get('/DatosPersonales', (req, res) => {
+    res.render('DatosPersonales'), {
+        titulo: "Esto son los datos personales",
+        descripcion: "Aqui estan los datos personales"
+    }
+})
+
+app.get('/Personalidad', (req, res) => {
+    res.render('Personalidad'), {
+        titulo: "Esta es mi personalidad",
+        descripcion: "Aqui va todo sobre mi"
+    }
+})
 
 //esta nuestra funcion, si el estado es 404 o no encotrado, devuelva el arrchivo 404.html
-app.use((req, res, next)=>{
-    res.status(404).sendFile(__dirname+'/public/404.html')
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
 })
 
 
